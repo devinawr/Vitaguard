f2a0b875aed3fc8e4f065fb6d909c0fd13baa107
Devina Wongkar <devinawongkar2@gmail.com>
2026-06-18 21:02:45 +0700

    NodeJS

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::resources([
     'consultations' => ConsultationController::class,
     'consultation-messages' => ConsultationMessageController::class,
 ]);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
